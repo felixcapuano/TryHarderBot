da328a8d378232e5a5d9080e32681dc20e6fee74
MammothOG <felix.capuano@gmail.com>
2020-04-15 17:49:33 +0200

    adding const file + gitignore file

---
+++ tryharderbot.py
@@ -0,0 +1,114 @@
+import fortnitepy
+import json
+import os
+
+from discord.ext import commands
+
+import const
+
+# region initialisation
+# ---------------------
+def get_device_auth_details():
+    if os.path.isfile(const.filename):
+        with open(const.filename, 'r') as fp:
+            return json.load(fp)
+    return {}
+
+def store_device_auth_details(email, details):
+    existing = get_device_auth_details()
+    existing[const.email] = details
+
+    with open(filename, 'w') as fp:
+        json.dump(existing, fp)
+
+device_auth_details = get_device_auth_details().get(const.email, {})
+
+fortnite_client = fortnitepy.Client(auth=fortnitepy.AdvancedAuth(
+                                            email=const.email,
+                                            password=const.password,
+                                            prompt_exchange_code=True,
+                                            delete_existing_device_auths=True,
+                                            **device_auth_details
+                                        )
+                                    )
+
+discord_bot = commands.Bot(command_prefix=const.PREFIX,
+                            description='My discord + fortnite bot!',
+                            case_insensitive=True
+                    )
+# region end 
+
+# region fortnite event
+# ---------------------
+@fortnite_client.event
+async def event_ready():
+    print('Fortnite client ready')
+    await discord_bot.start(const.discord_bot_token)
+
+@fortnite_client.event
+async def event_device_auth_generate(details, email):
+    store_device_auth_details(email, details)
+
+@fortnite_client.event
+async def event_logout():
+    await discord_bot.logout()
+
+@discord_bot.event
+async def on_ready():
+    print('Discord bot ready')
+# region end
+
+# region discord event
+# --------------------
+@discord_bot.event
+async def on_message(message):
+    if message.author == discord_bot.user:
+        return
+    
+    if message.channel.name == const.CHANNEL:
+        print('Received message from {0.author.display_name} | Content"{0.content}"'.format(message))
+        await discord_bot.process_commands(message)
+# region end
+
+# region checker
+# --------------
+def admin(ctx):
+    return ctx.message.author.id in const.admins
+
+def moderator(ctx):
+    return ctx.message.author.id in const.moderators or admin(ctx)
+# region end
+
+# region discord commands
+# -----------------------
+@discord_bot.command()
+@commands.check(admin)
+async def stop(ctx):
+    await discord_bot.logout()
+    print("stop")
+
+@discord_bot.command()
+async def join(ctx):
+    pass
+
+@discord_bot.command()
+async def up(ctx):
+    pass
+
+@discord_bot.command()
+@commands.check(moderator)
+async def upall(ctx):
+    pass
+
+@discord_bot.command()
+@commands.check(moderator)
+async def delete(ctx):
+    print("slt")
+
+@discord_bot.command()
+@commands.check(admin)
+async def delall(ctx):
+    print("delall")
+# region end
+
+fortnite_client.run()
